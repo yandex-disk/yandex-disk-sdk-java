737f4145cfa1e172a5b8a5403132e5cc952ed3d0
innik <yakovtsova@gmail.com>
2013-09-11 17:08:47 +0400

    MOBDISK-2314 Added WebdavSharingForbiddenException handling when read-only folder trying to publish

---
+++ sdk/src/com/yandex/disk/client/exceptions/WebdavSharingForbiddenException.java
@@ -0,0 +1,8 @@
+package com.yandex.disk.client.exceptions;
+
+public class WebdavSharingForbiddenException extends WebdavException{
+
+    public WebdavSharingForbiddenException(String detailMessage) {
+        super(detailMessage);
+    }
+}
