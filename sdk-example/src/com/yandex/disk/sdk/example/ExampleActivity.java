981dc14f256316e63beef9ff07d4ebfd9f0e9bc6
Oleg Okhotnikov <oleg@yandex-team.ru>
2013-09-27 17:37:05 +0400

    using Account Manager

--- sdk-example/src/com/yandex/disk/sdk/example/ExampleActivity.java
+++ sdk-example/src/com/yandex/disk/sdk/example/ExampleActivity.java
@@ -6,6 +6,13 @@
 
 package com.yandex.disk.sdk.example;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorDescription;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
@@ -18,7 +25,9 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,10 +35,18 @@ public class ExampleActivity extends FragmentActivity {
 
     private static String TAG = "ExampleActivity";
 
+    private static final int GET_ACCOUNT_CREDS_INTENT = 100;
+
     public static String FRAGMENT_TAG = "list";
 
-    // create your own client_id on oauth.yandex.ru
-    private static String AUTH_URL = "https://oauth.yandex.ru/authorize?response_type=token&client_id=ff90127313fd4378873d6b57914e8e11";
+    // create your own client id/secret pair with callback url on oauth.yandex.ru
+    public static final String CLIENT_ID = "82e97a6ea25547478f7824c572e7c625";
+    public static final String CLIENT_SECRET = "33073e7ca854421583445c0e792b8f6a";
+
+    public static final String ACCOUNT_TYPE = "com.yandex";
+    public static final String AUTH_URL = "https://oauth.yandex.ru/authorize?response_type=token&client_id="+CLIENT_ID;
+    private static final String ACTION_ADD_ACCOUNT = "com.yandex.intent.ADD_ACCOUNT";
+    private static final String KEY_CLIENT_SECRET = "clientSecret";
 
     public static String USERNAME = "example.username";
     public static String TOKEN = "example.token";
@@ -45,15 +62,19 @@ public class ExampleActivity extends FragmentActivity {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         String token = preferences.getString(TOKEN, null);
         if (token == null) {
-            new AuthDialogFragment().show(getSupportFragmentManager(), "auth");
+            getToken();
             return;
         }
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(android.R.id.content, new ListExampleFragment(), FRAGMENT_TAG)
-                    .commit();
+            startFragment();
         }
+    }
+
+    private void startFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new ListExampleFragment(), FRAGMENT_TAG)
+                .commit();
     }
 
     private void onLogin () {
@@ -65,21 +86,113 @@ public class ExampleActivity extends FragmentActivity {
             final String token = matcher.group(1);
             if (!TextUtils.isEmpty(token)) {
                 Log.d(TAG, "onLogin: token: "+token);
-                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
-                editor.putString(USERNAME, "");
-                editor.putString(TOKEN, token);
-                editor.commit();
+                saveToken(token);
             } else {
-                Log.w(TAG, "onRegistrationSuccess::empty token");
+                Log.w(TAG, "onRegistrationSuccess: empty token");
             }
         } else {
-            Log.w(TAG, "onRegistrationSuccess::token not found in return url");
+            Log.w(TAG, "onRegistrationSuccess: token not found in return url");
         }
+    }
+
+    private void saveToken(String token) {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.putString(USERNAME, "");
+        editor.putString(TOKEN, token);
+        editor.commit();
     }
 
     public void reloadContent() {
         ListExampleFragment fragment = (ListExampleFragment) getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG);
         fragment.restartLoader();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GET_ACCOUNT_CREDS_INTENT) {
+            if (resultCode == RESULT_OK) {
+                Bundle bundle = data.getExtras();
+                String name = bundle.getString(AccountManager.KEY_ACCOUNT_NAME);
+                String type = bundle.getString(AccountManager.KEY_ACCOUNT_TYPE);
+                Log.d(TAG, "GET_ACCOUNT_CREDS_INTENT: name="+name+" type="+type);
+                Account account = new Account(name, type);
+                getAuthToken(account);
+            }
+        }
+    }
+
+    private void getAuthToken(Account account) {
+        AccountManager systemAccountManager = AccountManager.get(getApplicationContext());
+        Bundle options = new Bundle();
+        options.putString(KEY_CLIENT_SECRET, CLIENT_SECRET);
+        systemAccountManager.getAuthToken(account, CLIENT_ID, options, this, new GetAuthTokenCallback(), null);
+    }
+
+    private void invalidateAuthToken(String authToken) {
+        AccountManager systemAccountManager = AccountManager.get(getApplicationContext());
+        systemAccountManager.invalidateAuthToken(ACCOUNT_TYPE, authToken);
+    }
+
+    private void getToken() {
+        AccountManager accountManager = AccountManager.get(getApplicationContext());
+        Account[] accounts = accountManager.getAccountsByType(ACCOUNT_TYPE);
+        Log.d(TAG, "accounts: "+(accounts != null ? accounts.length : null));
+
+        if (accounts != null && accounts.length > 0) {
+            // get the first account, for example (you must show the list and allow user to choose)
+            Account account = accounts[0];
+            Log.d(TAG, "account: "+account);
+            getAuthToken(account);
+            return;
+        }
+
+        Log.d(TAG, "No such accounts: "+ACCOUNT_TYPE);
+        for (AuthenticatorDescription authDesc : accountManager.getAuthenticatorTypes()) {
+            if (ACCOUNT_TYPE.equals(authDesc.type)) {
+                Log.d(TAG, "Starting "+ACTION_ADD_ACCOUNT);
+                Intent intent = new Intent(ACTION_ADD_ACCOUNT);
+                startActivityForResult(intent, GET_ACCOUNT_CREDS_INTENT);
+                return;
+            }
+        }
+
+        // no account manager for com.yandex
+        new AuthDialogFragment().show(getSupportFragmentManager(), "auth");
+    }
+
+    private class GetAuthTokenCallback implements AccountManagerCallback<Bundle> {
+        public void run(AccountManagerFuture<Bundle> result) {
+            try {
+                Bundle bundle = result.getResult();
+                Log.d(TAG, "bundle: "+bundle);
+
+                String message = (String) bundle.get(AccountManager.KEY_ERROR_MESSAGE);
+                if (message != null) {
+                    Toast.makeText(ExampleActivity.this, message, Toast.LENGTH_LONG).show();
+                }
+
+                Intent intent = (Intent) bundle.get(AccountManager.KEY_INTENT);
+                Log.d(TAG, "intent: "+intent);
+                if (intent != null) {
+                    // User input required
+                    startActivityForResult(intent, GET_ACCOUNT_CREDS_INTENT);
+                } else {
+                    String token = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+                    Log.d(TAG, "GetAuthTokenCallback: token="+token);
+                    saveToken(token);
+                    startFragment();
+                }
+            } catch (OperationCanceledException ex) {
+                Log.d(TAG, "GetAuthTokenCallback", ex);
+                Toast.makeText(ExampleActivity.this, ex.getMessage(), Toast.LENGTH_LONG).show();
+            } catch (AuthenticatorException ex) {
+                Log.d(TAG, "GetAuthTokenCallback", ex);
+                Toast.makeText(ExampleActivity.this, ex.getMessage(), Toast.LENGTH_LONG).show();
+            } catch (IOException ex) {
+                Log.d(TAG, "GetAuthTokenCallback", ex);
+                Toast.makeText(ExampleActivity.this, ex.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     public static class AuthDialogFragment extends DialogFragment {
